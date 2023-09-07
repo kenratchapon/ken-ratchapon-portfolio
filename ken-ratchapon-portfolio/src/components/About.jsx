a9ee50b67669d2522a365434b1fece19dd60ae82
Ken Ratchapon <ratchapon.pk@outlook.com>
2023-09-07 19:50:39 +0700

    no message

--- ken-ratchapon-portfolio/src/components/About.jsx
+++ ken-ratchapon-portfolio/src/components/About.jsx
@@ -8,7 +8,7 @@ import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from "../lib/data";
 
 const textAboutMe = `My name is Ratchapon Pukkham, also known as Ken, a recent Computer Science graduate from Chiang Mai University. 
-                    With a passion for frontend development, I have experience in a software house through a six-month cooperative education program. 
+                    With a passion for web development, I have experience in a software house through a six-month cooperative education program. 
                     I have a strong foundation in HTML, CSS, and JavaScript, along with expertise in frontend/full-stack frameworks. 
                     I strive for intuitive user experiences and continuously stay updated with the latest industry trends. 
                     Explore my portfolio website for examples of my work. Thank you for visiting!`
